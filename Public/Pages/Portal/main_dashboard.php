ac3ed13f992bd0ee10c7d228d45ac6559bbce577
TeamWebLux <team.webluxdigi@gmail.com>
2024-03-08 16:07:50 +0530

    Update main_dashboard.php

--- Public/Pages/Portal/main_dashboard.php
+++ Public/Pages/Portal/main_dashboard.php
@@ -13,7 +13,7 @@
     }
 
     // Check if there's a toast message set in session, display it, then unset
-    //print_r($_SESSION);
+    print_r($_SESSION);
     if (isset($_SESSION['toast'])) {
         $toast = $_SESSION['toast'];
         echoToastScript($toast['type'], $toast['message']);
