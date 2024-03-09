66137aae46089e567856e6764a3d44e56cdb9161
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-09 18:11:45 +0530

    Update formcomp.php

--- Public/Pages/Portal/Components/formcomp.php
+++ Public/Pages/Portal/Components/formcomp.php
@@ -1,65 +1,65 @@
 <?php 
 
-$name='<div class="form-group">
-<label class="form-label" for="Inputname">Name</label>
-<input type="text" name="name" class="form-control" id="exampleInputText1" value="" placeholder="Enter Name">
-</div>';
-$First_name='<div class="form-group mt-2">
-<label class="form-label" for="InputFirstname">Last name</label>
-<input type="text" name="fname" class="form-control" id="InputFirstname" aria-describedby="FirstHelp" placeholder="Enter First Name">
-</div>';
-$last_name='<div class="form-group mt-2">
-    <label class="form-label" for="Inputlastname">Last name</label>
-    <input type="text" name="lname" class="form-control" id="Inputlastname" aria-describedby="lastlHelp" placeholder="Enter Last Name">
-    </div>';
+// $name='<div class="form-group">
+// <label class="form-label" for="Inputname">Name</label>
+// <input type="text" name="name" class="form-control" id="exampleInputText1" value="" placeholder="Enter Name">
+// </div>';
+// $First_name='<div class="form-group mt-2">
+// <label class="form-label" for="InputFirstname">Last name</label>
+// <input type="text" name="fname" class="form-control" id="InputFirstname" aria-describedby="FirstHelp" placeholder="Enter First Name">
+// </div>';
+// $last_name='<div class="form-group mt-2">
+//     <label class="form-label" for="Inputlastname">Last name</label>
+//     <input type="text" name="lname" class="form-control" id="Inputlastname" aria-describedby="lastlHelp" placeholder="Enter Last Name">
+//     </div>';
 
-$email_id='<div class="form-group mt-2">
-<label class="form-label" for="Inputemail">Email address</label>
-<input type="email" name="email" class="form-control" id="Inputemail1" aria-describedby="emailHelp" placeholder="Enter Email">
-</div>';
-$Password='<div class="form-group mt-2">
-<label class="from-label" for="Password">Password</label>
-<input type="password" name="password" class="form-control" id="Inputpassword" aria-describedby="passwordhelp" placeholder="Enter Password">
-</div>';
+// $email_id='<div class="form-group mt-2">
+// <label class="form-label" for="Inputemail">Email address</label>
+// <input type="email" name="email" class="form-control" id="Inputemail1" aria-describedby="emailHelp" placeholder="Enter Email">
+// </div>';
+// $Password='<div class="form-group mt-2">
+// <label class="from-label" for="Password">Password</label>
+// <input type="password" name="password" class="form-control" id="Inputpassword" aria-describedby="passwordhelp" placeholder="Enter Password">
+// </div>';
 
-$url_input='<div class="form-group">
-<label class="form-label" for="exampleInputurl">Url Input</label>
-<input type="url" name="input" class="form-control" id="exampleInputurl" value="" placeholder="Enter Url">
-</div>';
-$Teliphone_Input='<div class="form-group">
-<label class="form-label" for="exampleInputphone">Teliphone Input</label>
-<input type="tel" name="telephone number" class="form-control" id="exampleInputphone" value="">
-</div>';
-$Number_Input=' <div class="form-group">
-<label class="form-label" for="exampleInputNumber1">Number Input</label>
-<input type="number" name="number" class="form-control" id="exampleInputNumber1" value="">
-</div>';
-$Date_Input='<div class="form-group">
-<label class="form-label" for="exampleInputdate">Date Input</label>
-<input type="date" name="date" class="form-control" id="exampleInputdate" value="">
-</div>';
-$Month_Input='<div class="form-group">
-<label class="form-label" for="exampleInputmonth">Month Input</label>
-<input type="month" name="month" class="form-control" id="exampleInputmonth" value="">
-</div>';
-$Week_Input='<div class="form-group">
-<label class="form-label" for="exampleInputweek">Week Input</label>
-<input type="week" name="week" class="form-control" id="exampleInputweek" value="">
-</div>';
-$Time_Input='<div class="form-group">
-<label class="form-label" for="exampleInputtime">Time Input</label>
-<input type="time" name="time" class="form-control" id="exampleInputtime" value="">
-</div>';
-$Date_and_Time_Input=' <div class="form-group">
-<label class="form-label" for="exampleInputdatetime">Date and Time Input</label>
-<input type="datetime-local" name="date and time" class="form-control" id="exampleInputdatetime" value="">
-</div>';
-$Example_textarea=' <div class="form-group">
-<label class="form-label" for="exampleFormControlTextarea1">Example textarea</label>
-<textarea name="textarea" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-</div>';
+// $url_input='<div class="form-group">
+// <label class="form-label" for="exampleInputurl">Url Input</label>
+// <input type="url" name="input" class="form-control" id="exampleInputurl" value="" placeholder="Enter Url">
+// </div>';
+// $Teliphone_Input='<div class="form-group">
+// <label class="form-label" for="exampleInputphone">Teliphone Input</label>
+// <input type="tel" name="telephone number" class="form-control" id="exampleInputphone" value="">
+// </div>';
+// $Number_Input=' <div class="form-group">
+// <label class="form-label" for="exampleInputNumber1">Number Input</label>
+// <input type="number" name="number" class="form-control" id="exampleInputNumber1" value="">
+// </div>';
+// $Date_Input='<div class="form-group">
+// <label class="form-label" for="exampleInputdate">Date Input</label>
+// <input type="date" name="date" class="form-control" id="exampleInputdate" value="">
+// </div>';
+// $Month_Input='<div class="form-group">
+// <label class="form-label" for="exampleInputmonth">Month Input</label>
+// <input type="month" name="month" class="form-control" id="exampleInputmonth" value="">
+// </div>';
+// $Week_Input='<div class="form-group">
+// <label class="form-label" for="exampleInputweek">Week Input</label>
+// <input type="week" name="week" class="form-control" id="exampleInputweek" value="">
+// </div>';
+// $Time_Input='<div class="form-group">
+// <label class="form-label" for="exampleInputtime">Time Input</label>
+// <input type="time" name="time" class="form-control" id="exampleInputtime" value="">
+// </div>';
+// $Date_and_Time_Input=' <div class="form-group">
+// <label class="form-label" for="exampleInputdatetime">Date and Time Input</label>
+// <input type="datetime-local" name="date and time" class="form-control" id="exampleInputdatetime" value="">
+// </div>';
+// $Example_textarea=' <div class="form-group">
+// <label class="form-label" for="exampleFormControlTextarea1">Example textarea</label>
+// <textarea name="textarea" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+// </div>';
 $Submit='<button type="submit" class="btn btn-primary">Submit</button>';
-$Cancel='<button type="submit" class="btn btn-danger">Cancel</button>';
+$Cancel='<button type="reset" class="btn btn-danger">Reset</button>';
 
 $Remember_me=' <div class="form-check">
 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3">
