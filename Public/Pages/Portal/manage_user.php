5069e7b887d103bffde5a7679ff30a9864d8c1ae
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-12 18:27:36 +0530

    Update manage_user.php

--- Public/Pages/Portal/manage_user.php
+++ Public/Pages/Portal/manage_user.php
@@ -132,9 +132,9 @@ if (in_array($role, ['Agent', 'Supervisor', 'Manager', 'Admin'])) {
                         <div class="card">
                             <div class="card-header">
                                 <h4 class="mb-0">User List</h4>
-                                <a href="./Add_User" style="text-decoration: none;">
+                                <!-- <a href="./Add_User" style="text-decoration: none;">
                                     <button type="button" class="btn btn-outline-info rounded-pill mt-2">Add User</button>
-                                </a>
+                                </a> -->
 
                             </div>
                             <?php
