1c8b52b680dd872421d9056764aa383f2de62c29
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-13 13:44:36 +0530

    Update see_users.php

--- Public/Pages/Portal/see_users.php
+++ Public/Pages/Portal/see_users.php
@@ -101,60 +101,65 @@
                         ?>
                             <div class="card-body">
                                 <div class="custom-table-effect table-responsive  border rounded">
-                                    <table class="table mb-0" id="example" data-toggle="data-table">
-                                        <thead>
-                                            <tr class="bg-white">
-                                                <?php
-                                                echo '<tr>
-                                            <th scope="col">ID</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Full Name</th>
-                                            <th scope="col">Password</th>
-                                            <th scope="col">Role</th>
-                                            <th scope="col">Created At</th>
-                                            <th scope="col">Last Login</th>
-                                            </tr>';
-                                                ?>
-                                                <thead>
-                                                <tbody>
-                                                    <?php
-                                                    while ($row = $result->fetch_assoc()) {
-                                                        echo "<tr>
-                                                    
-                                                    <td>{$row['id']}</td>
-                                                    <td>{$row['username']}</td>
-                                                    <td>{$row['name']}</td>
-                                                    <td>{$row['password']}</td>
-                                                    <td>{$row['role']}</td>
-                                                    <td>{$row['created_at']}</td> <!-- Consider if you really want to display passwords -->
-                                                    <td>{$row['last_login']}</td>
-                                                   
-                                                  </tr>";
-                                                    }
-                                                    ?>
 
-                                                </tbody>
-                                            <?php
+                                        <table id="example" class="table table-bordered table-hover display nowrap margin-top-10 w-p100">
+                                            <thead>
+                                                <tr>
+                                                    <th>Transaction Type</th>
+                                                    <th>Recharge</th>
+                                                    <th>Redeem</th>
+                                                    <th>Excess Amount</th>
+                                                    <th>Bonus Amount</th>
+                                                    <th>Free Play</th>
+                                                    <th>Platform Name</th>
+                                                    <th>Page Name</th>
+                                                    <th>CashApp Name</th>
+                                                    <th>Timestamp</th>
+                                                    <th>Username</th>
+                                                    <th>By</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach ($results as $row) : ?>
+                                                    <tr>
+                                                        <td class="<?= ($row['type'] === 'Debit') ? 'Debit' : 'Credit' ?>">
+                                                            <?= $row['type'] ?>
+                                                        </td>
+                                                        <td><?= $row['recharge'] ?></td>
+                                                        <td><?= $row['redeem'] ?></td>
+                                                        <td><?= $row['excess'] ?></td>
+                                                        <td><?= $row['bonus'] ?></td>
+                                                        <td><?= $row['freepik'] ?></td>
 
-                                            // End table
-                                            echo '</table>';
-                                        } else {
-                                            echo "0 results";
-                                        }
+                                                        <td><?= $row['platform'] ?></td>
+                                                        <td><?= $row['page'] ?></td>
+                                                        <td><?= $row['cashapp'] ?></td>
 
-                                        // Close connection
-                                        $conn->close();
-                                            ?>
+                                                        <td><?= $row['created_at'] ?></td>
+                                                        <td><?= $row['username'] ?></td>
+                                                        <td><?= $row['by_u'] ?></td>
+                                                    </tr>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    <?php
+                                } else {
+                                    echo "0 results";
+                                }
+
+                                // Close connection
+                                $conn->close();
+                                    ?>
 
 
 
+                                    </div>
                                 </div>
                             </div>
                     </div>
+
                 </div>
-
             </div>
-        </div>
 
 
 
@@ -162,10 +167,10 @@
 
 
 
-        <?
-        include("./Public/Pages/Common/footer.php");
-        // print_r($_SESSION);
-        ?>
+            <?
+            include("./Public/Pages/Common/footer.php");
+            // print_r($_SESSION);
+            ?>
 
     </main>
     <!-- Wrapper End-->
