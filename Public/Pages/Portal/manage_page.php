582d681d5e742f2b857a4a256d95a2f8654c9023
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-13 16:58:25 +0530

    update

--- Public/Pages/Portal/manage_page.php
+++ Public/Pages/Portal/manage_page.php
@@ -132,7 +132,7 @@ if (in_array($role, ['Agent', 'Supervisor', 'Manager', 'Admin'])) {
                             ?>
                                 <div class="card-body">
                                     <div class="custom-table-effect table-responsive  border rounded">
-                                        <table class="table mb-0" id="datatable" data-toggle="data-table">
+                                    <table class="table mb-0" id="example" >
                                             <thead>
                                                 <tr class="bg-white">
                                                     <?php
