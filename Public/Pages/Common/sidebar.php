06c72d6855d66be558d06bbf9509ad67ed0de9cb
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-13 21:38:49 +0530

    Update sidebar.php

--- Public/Pages/Common/sidebar.php
+++ Public/Pages/Common/sidebar.php
@@ -420,6 +420,20 @@
                                         <span class="item-name">See Deposits</span>
                                     </a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link  <?php if ($_SERVER['REQUEST_URI'] == '/CustCount/index.php/See_All_Reports') echo 'active';  ?>" href="../index.php/See_All_Reports">
+                                        <i class="icon">
+                                            <svg class="icon-10" width="10" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <g>
+                                                    <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                                </g>
+                                            </svg>
+                                        </i>
+                                        <i class="sidenav-mini-icon" data-bs-toggle="tooltip" title="Calender" data-bs-placement="right">C</i>
+                                        <span class="item-name">See All Reports</span>
+                                    </a>
+                                </li>
+
                         </ul>
                     <?php endif; ?>
 
