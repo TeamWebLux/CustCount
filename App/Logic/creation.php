b7ca4a9f7c0dde1660cafadb8e850a9b158c6c8a
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-13 18:40:27 +0530

    Update creation.php

--- App/Logic/creation.php
+++ App/Logic/creation.php
@@ -113,7 +113,7 @@ class Creation
 
             } elseif ($type == "Redeem") {
                 $openingBalance = $previousClosingBalance;
-                if ($openingBalance <= $amount) {
+                if ($openingBalance >= $amount) {
                     $closingBalance = $openingBalance - $amount; // Closing balance will be the opening balance plus the recharge amount
                 } else {
                     $_SESSION['toast'] = ['type' => 'error', 'message' => 'Not Enough Money to do Transaction.'];
